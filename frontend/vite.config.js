c11542b9b61f100cad04e1cd6e9192ac11276720
sunwooxxi <bibless03@gmail.com>
2026-06-29 14:58:06 +0900

    feat/frontend-react-first-commit

---
+++ frontend/vite.config.js
@@ -0,0 +1,7 @@
+// /Users/sniperfactory/Desktop/AI-Captcha/frontend/vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
+});
